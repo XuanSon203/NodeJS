b04444c12f7b9aea64a9642b055c405a22497ac0
Xuanson <pxuanson203@gmail.com>
2025-04-12 22:36:11 +0700

    Kiem tra quyen

--- middlewares/admin/authMiddleware.js
+++ middlewares/admin/authMiddleware.js
@@ -1,5 +1,6 @@
 const systemConfig = require("../../config/system");
 const Account = require("../../model/AccountModel");
+const Role = require('../../model/RolesModel');
 module.exports.requireAuth = async (req, res, next) => {
   const token = req.cookies.token;
   if (!token) {
@@ -7,11 +8,16 @@ module.exports.requireAuth = async (req, res, next) => {
   } else {
     const user = await Account.findOne({
       token: token,
-    });
-    if (user) {
-      next();
-    } else {
+    }).select("-password");
+    if (!user) {
       res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
+    } else {
+      const role = await Role.findOne({
+        _id:user.role_id
+      }).select("title permissions")
+      res.locals.user = user; // res tạo ra một biến toàn cục trả về giao diện 
+      res.locals.role = role
+      next();
     }
     
   }
